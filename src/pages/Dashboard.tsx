e23f2f6fcbca15a4cdc380639fe5c856a15f9df3
Yum-wu <1172989563@qq.com>
2026-06-20 19:09:36 +0800

    fix: remove unused useState imports after useViewStore migration

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDashboardStats } from '../hooks/useDashboardStats';
 import { useSystemHealth } from '../hooks/useSystemHealth';
