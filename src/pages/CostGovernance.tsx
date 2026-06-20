6b18bba9985849e9cb4fb2730fa77721ff93cb2b
Yum-wu <1172989563@qq.com>
2026-06-20 19:09:36 +0800

    fix: remove unused useState imports after useViewStore migration

--- src/pages/CostGovernance.tsx
+++ src/pages/CostGovernance.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'sonner';
 import { useCostData } from '../hooks/useCostData';
