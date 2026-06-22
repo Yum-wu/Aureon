ec1e6c482cdd36ad5ca8733e93f2c8d03f3b1e94
Yum-wu <1172989563@qq.com>
2026-06-22 23:28:18 +0800

    fix: suppress eslint any warning in build-verify

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { QueryProvider } from "./providers/QueryProvider";
 
 /* build-verify: canvas-nav-toast-2026 */
 if (typeof window !== 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).__AUREON_BUILD__ = 'canvas-nav-toast-2026';
 }
 
